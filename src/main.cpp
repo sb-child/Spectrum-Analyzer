034731a013b93d3b2e20f1289049d354eca0aff3
sbchild <a15315862177@163.com>
2023-05-03 12:15:32 +0800

    license

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+// Copyright 2023 sbchild
+
+// Licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http: // www.apache.org/licenses/LICENSE-2.0
+
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <Wire.h>
